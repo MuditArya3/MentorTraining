c27abdce7e87085288162881950ce804733fcb83
MuditArya3 <Mudit.Arya@bhavnacorp.com>
2022-09-16 17:57:19 +0530

    first commit

---
+++ signup/script.js
@@ -0,0 +1,16 @@
+function myFunction() {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+  }
+
+
+ function myfunction(){
+    var n=document.getElementById("name1").value;
+    var p=document.getElementById("pass").value;
+  
+    alert("Login Successfull, Login Id is: "+ n +" , Password is: "+p)
+ }
+
+ function hello(){
+    alert("Connect Wise is a great team. Join us and you will learn alot. We wish you a great journey!")
+ }
