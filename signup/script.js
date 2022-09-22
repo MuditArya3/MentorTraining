56d56b6bdd6424479b79ec4e3169f795c5f80c59
MuditArya3 <110027831+MuditArya3@users.noreply.github.com>
2022-09-22 18:04:53 +0530

    2nd commit

--- signup/script.js
+++ signup/script.js
@@ -5,12 +5,37 @@ function myFunction() {
 
 
  function myfunction(){
-    var n=document.getElementById("name1").value;
-    var p=document.getElementById("pass").value;
+
+   //  var n=document.getElementById("name1").value;
+   //  var p=document.getElementById("pass").value;
   
-    alert("Login Successfull, Login Id is: "+ n +" , Password is: "+p)
+     alert("Login Successfull, Hooray!! ")
+  
  }
 
  function hello(){
     alert("Connect Wise is a great team. Join us and you will learn alot. We wish you a great journey!")
  }
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    alert("Login Successfull, Login Id is: "+ n +" , Password is: "+p)
